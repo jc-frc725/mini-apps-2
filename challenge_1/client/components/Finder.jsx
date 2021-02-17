58fe476681fdaf485b4a774ff08c637feafdfd87
Justin Chow <hrll71990@gmail.com>
2021-02-17 12:02:49 -0800

    simple search function

--- challenge_1/client/components/Finder.jsx
+++ challenge_1/client/components/Finder.jsx
@@ -2,8 +2,26 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
 const Finder = () => {
+  const [query, setQuery] = useState('');
+
+  const handleSubmit = (event) => {
+    axios.get(`/events?q=${query}`)
+      .then(({ data }) => {
+        console.log(data);
+      })
+      .catch(error => console.error(error));
+    event.preventDefault();
+  }
+
   return (
-    <div>Hello world</div>
+    <div>
+      <h2>Find events</h2>
+      <form onSubmit={handleSubmit}>
+        <h4>Enter a search term:</h4>
+        <input type="text" value={query} onChange={() => setQuery(event.target.value)}></input>
+        <button>Search</button>
+      </form>
+    </div>
   );
 };
 
