343063243deed47cc5542907b4480e372ffea1d3
Justin Chow <hrll71990@gmail.com>
2021-02-17 11:39:08 -0800

    init

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,2 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Finder from './components/Finder';
 
+ReactDOM.render(<Finder />, document.getElementById('app'));
 
---
+++ challenge_1/client/components/Finder.jsx
@@ -0,0 +1,10 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+const Finder = () => {
+  return (
+    <div>Hello world</div>
+  );
+};
+
+export default Finder;
